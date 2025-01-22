37be29c323a9e1d74b7f39938ca5a57962199580
MJSuttles <rolltiderolldad@gmail.com>
2025-01-21 21:44:56 -0600

    seeded Product and ProductType data

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,11 +1,20 @@
-﻿
-//create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
+﻿using System;
 
-//create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
+List<Product> products = new List<Product>
+{
+    new Product() { Name = "Whispers of the Eternal Sky", Price = 15.99m, ProductTypeId = 100 },
+    new Product() { Name = "Golden Melody Acoustic Guitar", Price = 249.99m, ProductTypeId = 200 },
+    new Product() { Name = "The Enchanted Forest Chronicles", Price = 12.49m, ProductTypeId = 101 },
+    new Product() { Name = "SilverTone Alto Saxophone", Price = 599.50m, ProductTypeId = 201 },
+    new Product() { Name = "Beyond the Horizon: A Journey Untold", Price = 18.75m, ProductTypeId = 102 },
+    new Product() { Name = "HarmonyKeys Digital Piano", Price = 429.00m, ProductTypeId = 202 }
+};
 
-//put your greeting here
-
-//implement your loop here
+List<ProductType> producttype = new List<ProductType>
+{
+    new ProductType() { Title = "Book", Id = 1 },
+    new ProductType() { Title = "Musical Instrument", Id = 2 }
+};
 
 void DisplayMenu()
 {
